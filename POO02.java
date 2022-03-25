db33e23cfc02ff363fe02f4f61ff23324a26ea36
Agus Lucotti <93104880+LucottiA@users.noreply.github.com>
2022-03-25 00:55:18 -0300

    Update POO02.java

--- POO02.java
+++ POO02.java
@@ -16,19 +16,19 @@ package luco;
  */
 public class POO02 {
 
-    /** VER VIDEO POR DIOS https://www.youtube.com/watch?v=_6dpBMVoSmU&list=PLgwlfcqa5h3y1P5W-X13ZD-s1ItKqXcNk&index=6&ab_channel=EggEducaci%C3%B3n
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
    
         POO02Circunferencia c1 = new POO02Circunferencia(); //crear objeto?
         c1.crearCircunferencia(5.35); //determinar nueva circunferencia ?
-        System.out.println("Radio: " + c1.getRadio()); // imprimir el radio nuevo que era privado?
+        System.out.println("Radio: " + c1.getRadio()); // imprimir el radio nuevo que era privado
         System.out.println("Area: "+ c1.getArea()); // imprimir el resultado del calculo
         System.out.println("Perimetro: "+ c1.getPerimetro()); //imprimir resultado calculo
         
-        c1.setRadio(4);//??? ni idea ver videos del pelado
-        //puede ser que estoy estableciendo un nuevo valor de radio? con set porque es privado
+        c1.setRadio(4);
+        
          System.out.println("Radio: " + c1.getRadio()); // 
         System.out.println("Area: "+ c1.getArea()); // 
         System.out.println("Perimetro: "+ c1.getPerimetro()); //
